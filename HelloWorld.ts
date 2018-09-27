a2f3a2d1c2e36345bf7c0abc23ce02bbb48ca288
Gene Culling <culling66@gmail.com>
2018-09-28 11:50:49 +1200

    Init config

---
+++ HelloWorld.ts
@@ -0,0 +1,9 @@
+class Startup {
+    public static main(): number {
+        console.log('Hello World');
+        console.log('So begins the Sidescrolling Adventure in Typescript!');
+
+        return 0;
+    }
+}
+Startup.main();
