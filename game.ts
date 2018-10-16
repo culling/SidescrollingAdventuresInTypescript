1e4f2486eeafa665215505f90a5fc373de36d3e2
Gene Culling <culling66@gmail.com>
2018-10-16 20:26:14 +1300

    set the gravity and friction inside the actors

--- game.ts
+++ game.ts
@@ -1,21 +1,85 @@
 
 let canvas      =  <HTMLCanvasElement>document.getElementById('canvas') ;
 let context     = canvas.getContext("2d");
-let gravity     = 1;
-let friction    = 0.9;
-
+//let gravity     = 1;
+//let friction    = 0.9;
+/*
 let controller = {
     left: false,
     right: false,
     up: false
 }
-
+*/
 
 var map = {
+    enemies: [
+        {
+            title:  "slime1",
+            controller:{
+                left:   false,
+                right:  false,
+                up:     false
+            },
+            x:          200,
+            y:          canvas.height - 200,
+            jumping:    false,
+            color:      "red",
+            height:     20,
+            width:      20,
+            right:      this.x + this.width,
+            left:       this.x,
+            gravity:    1,
+            friction:   0.9,
+            xVelocity:  0,
+            yVelocity:  0,
+            defaultMoveX: 0.2,
+            defaultMoveY: 20,
+            jumpHeight: 20,        
+            getBottom: function(){
+                return this.y + this.height;
+            },
+            setBottom: function( newBottom ){
+                this.y = newBottom - this.height;
+            },
+            getTop: function(){
+                return this.y;
+            },
+            move: function(){
+                let controller = this.controller;
+                if(controller.up && !this.jumping){
+                    this.yVelocity -= this.defaultMoveY;
+                    this.jumping = true;
+                }
+            
+                if(controller.left){
+                    this.xVelocity -= this.defaultMoveX;
+                }
+                if(controller.right){
+                    this.xVelocity += this.defaultMoveX;
+                }
+            
+                this.yVelocity += this.gravity;
+            
+                this.x += this.xVelocity;
+                this.y += this.yVelocity;
+            
+                this.xVelocity *= this.friction;
+                this.yVelocity *= this.friction;
+            
+            },
+            draw: function(){
+                context.fillStyle = this.color;
+                //context.arc(this.x, this.y, this.width/2, 0, 360);
+                context.fillRect(this.x, this.y, this.width, this.height);
+            }
+        }
+    ],
     obstacles: [
     {
+        title:  "ground",
         x:      0,
-        y:      canvas.height - 100,
+        y:      canvas.height - 20,
+        color:  "black",
         height: canvas.height,
         width:  canvas.width,
         right:  this.x + this.width,
@@ -27,7 +91,26 @@ var map = {
             return this.y;
         },
         draw: function(){
-            context.fillStyle = 'black';
+            context.fillStyle = this.color;
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    },{
+        title:  "ledge1",
+        x:      80,
+        y:      canvas.height - 80,
+        color:  "black",
+        height: canvas.height,
+        width:  canvas.width,
+        right:  this.x + this.width,
+        left:   this.x,
+        getBottom: function(){
+            return this.y + this.height;
+        },
+        getTop: function(){
+            return this.y;
+        },
+        draw: function(){
+            context.fillStyle = this.color;
             context.fillRect(this.x, this.y, this.width, this.height);
         }
     }
@@ -35,6 +118,24 @@ var map = {
     draw: function(){
         for(var obstacle of this.obstacles){
             obstacle.draw();
+        }
+        for(var enemy of this.enemies){
+            enemy.draw();
+        }
+    },
+    moveEnemies: function(){
+        for(var enemy of this.enemies){
+            console.log(enemy);
+            enemy.move();
+
+            
+            var currentObstacle = map.getOntop(enemy);
+
+            if(currentObstacle != null){
+                enemy.jumping = false;
+                enemy.setBottom( currentObstacle.getTop() );
+                enemy.yVelocity = 0;
+            }
         }
     },
     getOntop: function(actor){
@@ -49,14 +150,19 @@ var map = {
 function isInside(actor, obstacle){
     var inX = (actor.x > obstacle.x )&& (actor.x < obstacle.x + obstacle.width );
     var inY = ( actor.getBottom() > obstacle.getTop() )&& (actor.getBottom() > obstacle.getTop() );
-    console.log("Actor Bottom: " + actor.getBottom()    );
-    console.log("Obstacle Top: " + obstacle.y);
+    //console.log("Actor Bottom: " + actor.getBottom()    );
+    //console.log("Obstacle Top: " + obstacle.y);
 
     return (inX && inY);
 }
 
 
 var box = {
+    controller: {
+        left: false,
+        right: false,
+        up: false
+    },
     x:          100,
     y:          10,
     xVelocity:  0,
@@ -70,6 +176,8 @@ var box = {
     right:      this.x+ this.width,
     color:      'blue',
     jumping:    false,
+    gravity:    1,
+    friction:   0.9,
     getBottom: function(){
         return this.y + this.height;
     },
@@ -78,6 +186,29 @@ var box = {
     },
     getTop: function(){
         return this.y;
+    },
+    move: function(){
+        let controller = this.controller;
+
+        if(controller.up && !this.jumping){
+            this.yVelocity -= this.defaultMoveY;
+            this.jumping = true;
+        }
+    
+        if(controller.left){
+            this.xVelocity -= this.defaultMoveX;
+        }
+        if(controller.right){
+            this.xVelocity += this.defaultMoveX;
+        }
+    
+        this.yVelocity += this.gravity;
+    
+        this.x += this.xVelocity;
+        this.y += this.yVelocity;
+    
+        this.xVelocity *= this.friction;
+        this.yVelocity *= this.friction;    
     },
     draw: function() {
         //console.log("Ball Draw function hit!");
@@ -91,15 +222,15 @@ window.addEventListener("keydown", function(e){
     console.log(e);
     switch(e.key){
         case"ArrowRight":{
-            controller.right = true;
+            box.controller.right = true;
             break;
         }
         case"ArrowLeft":{
-            controller.left = true;
+            box.controller.left = true;
             break;
         }
         case"ArrowUp":{
-            controller.up = true;
+            box.controller.up = true;
             break;
         }
 
@@ -113,15 +244,15 @@ window.addEventListener("keyup", function(e){
     console.log(e);
     switch(e.key){
         case"ArrowRight":{
-            controller.right = false;
+            box.controller.right = false;
             break;
         }
         case"ArrowLeft":{
-            controller.left = false;
+            box.controller.left = false;
             break;
         }
         case"ArrowUp":{
-            controller.up = false;
+            box.controller.up = false;
             break;
         }
 
@@ -132,26 +263,11 @@ window.addEventListener("keyup", function(e){
 });
 
 function gameLoop(){
-    if(controller.up && !box.jumping){
-        box.yVelocity -= box.defaultMoveY;
-        box.jumping = true;
-    }
 
-    if(controller.left){
-        box.xVelocity -= box.defaultMoveX;
-    }
-    if(controller.right){
-        box.xVelocity += box.defaultMoveX;
-    }
 
-    box.yVelocity += gravity;
-
-    box.x += box.xVelocity;
-    box.y += box.yVelocity;
-
-    box.xVelocity *= friction;
-    box.yVelocity *= friction;
-
+    box.move();
+    map.moveEnemies();
+    
 
     var currentObstacle = map.getOntop(box);
 
