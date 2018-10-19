1991f9ee2a4971afb55957ec974f8faf57679973
Gene Culling <culling66@gmail.com>
2018-10-19 19:33:21 +1300

    Moved files around and cleaned up

--- HelloWorld.ts
+++
@@ -1,9 +0,0 @@
-class Startup {
-    public static main(): number {
-        console.log('Hello World');
-        console.log('So begins the Sidescrolling Adventure in Typescript!');
-
-        return 0;
-    }
-}
-Startup.main();
